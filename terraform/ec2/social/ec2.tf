c2e92fa85de95c4e416901e267cbe0582895d7c9
Mike Plemmons <mikeplemmons@gmail.com>
2022-11-28 13:14:50 -0500

    block port 22
    - AWS SSM access is used to get shell access

--- terraform/ec2/social/ec2.tf
+++ terraform/ec2/social/ec2.tf
@@ -27,12 +27,12 @@ resource "aws_security_group" "social" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#  ingress {
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 
   lifecycle {
     create_before_destroy = true
