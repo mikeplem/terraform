ecc8a1ab6a3462ca997d0bc95bc52caba36b87ff
Mike Plemmons <mikeplemmons@gmail.com>
2023-10-15 15:27:41 -0400

    import cloudfront terraform

--- terraform/s3/n1mtp.com/cloudfront.tf
+++ terraform/s3/n1mtp.com/cloudfront.tf
@@ -1,45 +1,61 @@
 resource "aws_cloudfront_distribution" "website" {
-  origin {
-    domain_name = aws_s3_bucket.website.bucket_regional_domain_name
-    origin_id   = "n1mtp.com"
-  }
-
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
-
-  aliases = ["www.n1mtp.com"]
-
+  aliases                         = ["n1mtp.com"]
+  comment                         = null
+  default_root_object             = null
+  enabled                         = true
+  http_version                    = "http2"
+  is_ipv6_enabled                 = true
+  price_class                     = "PriceClass_100"
+  retain_on_delete                = false
+  tags                            = {}
+  tags_all                        = {}
+  wait_for_deployment             = true
+  web_acl_id                      = null
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "n1mtp.com"
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 31536000
-    default_ttl            = 31536000
-    max_ttl                = 31536000
-    compress               = true
+    allowed_methods            = ["GET", "HEAD"]
+    cache_policy_id            = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+    cached_methods             = ["GET", "HEAD"]
+    compress                   = true
+    default_ttl                = 0
+    field_level_encryption_id  = null
+    max_ttl                    = 0
+    min_ttl                    = 0
+    origin_request_policy_id   = null
+    realtime_log_config_arn    = null
+    response_headers_policy_id = null
+    smooth_streaming           = false
+    target_origin_id           = aws_s3_bucket_website_configuration.website.website_endpoint
+    trusted_key_groups         = []
+    trusted_signers            = []
+    viewer_protocol_policy     = "redirect-to-https"
   }
-
+  origin {
+    connection_attempts      = 3
+    connection_timeout       = 10
+    domain_name              = aws_s3_bucket_website_configuration.website.website_endpoint
+    origin_access_control_id = null
+    origin_id                = aws_s3_bucket_website_configuration.website.website_endpoint
+    origin_path              = null
+    custom_origin_config {
+      http_port                = 80
+      https_port               = 443
+      origin_keepalive_timeout = 5
+      origin_protocol_policy   = "http-only"
+      origin_read_timeout      = 30
+      origin_ssl_protocols     = ["TLSv1.2"]
+    }
+  }
   restrictions {
     geo_restriction {
+      locations        = []
       restriction_type = "none"
     }
   }
-
   viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate_validation.website.certificate_arn
-    ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2018"
+    acm_certificate_arn            = aws_acm_certificate.website.arn
+    cloudfront_default_certificate = false
+    iam_certificate_id             = null
+    minimum_protocol_version       = "TLSv1.2_2021"
+    ssl_support_method             = "sni-only"
   }
-
 }
