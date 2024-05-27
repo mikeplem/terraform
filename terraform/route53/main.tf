45922d251356e8430ed4b753a82fde84078c0681
Mike Plemmons <mikeplemmons@gmail.com>
2024-05-27 10:51:06 -0400

    update DNS records
    - follow a better pattern for record creation as this allows alias records to be created

--- terraform/route53/main.tf
+++ terraform/route53/main.tf
@@ -3,11 +3,22 @@ resource "aws_route53_zone" "n1mtp_com" {
 }
 
 resource "aws_route53_record" "dns" {
-    for_each = local.recordsets
+  for_each = local.recordsets
 
-    zone_id = aws_route53_zone.n1mtp_com.zone_id
-    name    = each.value.name
-    type    = each.value.type
-    ttl     = 300
-    records = [each.value.record]
+  zone_id = aws_route53_zone.n1mtp_com.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = lookup(each.value, "ttl", null)
+  records = try(each.value.record, null)
+
+
+  dynamic "alias" {
+    for_each = length(keys(lookup(each.value, "alias", {}))) == 0 ? [] : [true]
+
+    content {
+      name                   = each.value.alias.name
+      zone_id                = var.cloudfront_zone_id
+      evaluate_target_health = lookup(each.value.alias, "evaluate_target_health", false)
+    }
+  }
 }
--- terraform/route53/vars.tf
+++ terraform/route53/vars.tf
@@ -1,125 +1,152 @@
 locals {
   # convert from list to map with unique keys
-  recordsets        = { for rs in var.records : join(" ", compact(["${rs.name}_${rs.type}"])) => rs }
+  recordsets = { for rs in var.records : join(" ", compact(["${rs.name}_${rs.type}"])) => rs }
+}
+
+variable "cloudfront_zone_id" {
+  default = "Z2FDTNDATAQYW2"
 }
 
 variable "records" {
-    type = set(object({
-        name = string
-        record = string
-        type = string
-    }))
-
-    default = [
-        {
-            name = ""
-            record = "165.227.88.197"
-            type = "A"
-        },
-        {
-            name = ""
-            record = "10 mx.hover.com.cust.hostedemail.com"
-            type = "MX"
-        },
-        {
-            name = ""
-            record = "v=spf1 include:_spf.hostedemail.com include:hover.com ~all"
-            type = "TXT"
-        },
-        {
-            name = "_dmarc"
-            record = "v=DMARC1; p=quarantine; rua=mailto:mike@n1mtp.com; ruf=mailto:mike@n1mtp.com; fo=1;"
-            type = "TXT"
-        },
-        {
-            name = "cups"
-            record = "192.168.1.237"
-            type = "A"
-        },
-        {
-            name = "files.social"
-            record = "34.233.156.231"
-            type = "A"
-        },
-        {
-            name = "frigate"
-            record = "192.168.1.209"
-            type = "A"
-        },
-        {
-            name = "git"
-            record = "100.64.1.1"
-            type = "A"
-        },
-        {
-            name = "hass"
-            record = "13ittluc6noru76u7dkg0rkdpgsm817l.ui.nabu.casa"
-            type = "CNAME"
-        },
-        {
-            name = "_acme-challenge.hass.n1mtp.com"
-            record = "_acme-challenge.13ittluc6noru76u7dkg0rkdpgsm817l.ui.nabu.casa"
-            type = "CNAME"
-        },
-        {
-            name = "hasswg"
-            record = "100.64.1.5"
-            type = "A"
-        },
-        {
-            name = "homeassistant"
-            record = "192.168.1.141"
-            type = "A"
-        },
-        {
-            name = "local"
-            record = "127.0.0.1"
-            type = "A"
-        },
-        {
-            name = "mail"
-            record = "mail.hover.com.cust.hostedemail.com"
-            type = "CNAME"
-        },
-        {
-            name = "nas"
-            record = "192.168.1.40"
-            type = "A"
-        },
-        {
-            name = "owncloud"
-            record = "159.203.121.241"
-            type = "A"
-        },
-        {
-            name = "setup"
-            record = "192.168.1.1"
-            type = "A"
-        },
-        {
-            name = "social"
-            record = "34.233.156.231"
-            type = "A"
-        },
-        {
-            name = "tail"
-            record = "165.227.88.197"
-            type = "A"
-        },
-        {
-            name = "ubnt"
-            record = "192.168.1.1"
-            type = "A"
-        },
-        {
-            name = "unifi"
-            record = "192.168.1.230"
-            type = "A"
-        },
-        {
-            name = "www"
-            record = "n1mtp.com"
-            type = "CNAME"
-        }
-    ]
+  default = [
+    {
+      name = ""
+      type = "A"
+      alias = {
+        name = "d1486w975c4gqn.cloudfront.net"
+      }
+    },
+    {
+      name   = ""
+      record = ["10 mx.hover.com.cust.hostedemail.com"]
+      type   = "MX"
+      ttl    = 86400
+    },
+    {
+      name   = ""
+      record = ["v=spf1 include:_spf.hostedemail.com include:hover.com ~all"]
+      type   = "TXT"
+      ttl    = 86400
+    },
+    {
+      name   = "_dmarc"
+      record = ["v=DMARC1; p=quarantine; rua=mailto:mike@n1mtp.com; ruf=mailto:mike@n1mtp.com; fo=1;"]
+      type   = "TXT"
+      ttl    = 86400
+    },
+    {
+      name   = "cups"
+      record = ["192.168.1.237"]
+      type   = "A"
+      ttl    = 86400
+    },
+    {
+      name   = "files.social"
+      record = ["34.233.156.231"]
+      type   = "A"
+      ttl    = 86400
+    },
+    {
+      name   = "frigate"
+      record = ["192.168.1.209"]
+      type   = "A"
+      ttl    = 86400
+    },
+    {
+      name   = "git"
+      record = ["100.64.1.1"]
+      type   = "A"
+      ttl    = 86400
+    },
+    {
+      name   = "hass"
+      record = ["13ittluc6noru76u7dkg0rkdpgsm817l.ui.nabu.casa"]
+      type   = "CNAME"
+      ttl    = 86400
+    },
+    {
+      name   = "_acme-challenge.hass.n1mtp.com"
+      record = ["_acme-challenge.13ittluc6noru76u7dkg0rkdpgsm817l.ui.nabu.casa"]
+      type   = "CNAME"
+      ttl    = 86400
+    },
+    {
+      name   = "hasswg"
+      record = ["100.64.1.5"]
+      type   = "A"
+      ttl    = 86400
+    },
+    {
+      name   = "homeassistant"
+      record = ["192.168.1.141"]
+      type   = "A"
+      ttl    = 86400
+    },
+    {
+      name   = "local"
+      record = ["127.0.0.1"]
+      type   = "A"
+      ttl    = 86400
+    },
+    {
+      name   = "mail"
+      record = ["mail.hover.com.cust.hostedemail.com"]
+      type   = "CNAME"
+      ttl    = 86400
+    },
+    {
+      name   = "nas"
+      record = ["192.168.1.40"]
+      type   = "A"
+      ttl    = 86400
+    },
+    {
+      name   = "owncloud"
+      record = ["159.203.121.241"]
+      type   = "A"
+      ttl    = 86400
+    },
+    {
+      name   = "setup"
+      record = ["192.168.1.1"]
+      type   = "A"
+      ttl    = 86400
+    },
+    {
+      name   = "social"
+      record = ["34.233.156.231"]
+      type   = "A"
+      ttl    = 86400
+    },
+    {
+      name   = "tail"
+      record = ["165.227.88.197"]
+      type   = "A"
+      ttl    = 86400
+    },
+    {
+      name   = "twc"
+      record = ["192.168.1.141"]
+      type   = "A"
+      ttl    = 86400
+    },
+    {
+      name   = "ubnt"
+      record = ["192.168.1.1"]
+      type   = "A"
+      ttl    = 86400
+    },
+    {
+      name   = "unifi"
+      record = ["192.168.1.230"]
+      type   = "A"
+      ttl    = 86400
+    },
+    {
+      name   = "www"
+      record = ["n1mtp.com."]
+      type   = "CNAME"
+      ttl    = 86400
+    }
+  ]
 }
